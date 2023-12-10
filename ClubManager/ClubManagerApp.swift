6ff1a217cb8a6042e1f260bf29fc7580f8f7e541
김상진, Kim Sang Jin <62230118+sangjinKIM93@users.noreply.github.com>
2023-12-10 21:01:52 +0900

    Initial Commit

---
+++ ClubManager/ClubManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClubManagerApp.swift
+//  ClubManager
+//
+//  Created by 김상진 on 12/10/23.
+//
+
+import SwiftUI
+
+@main
+struct ClubManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
